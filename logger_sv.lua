bcbd6f8cc00f335e515fc557928c1696c67f01ed
heap <68669985+heap-v@users.noreply.github.com>
2022-04-16 22:13:31 +0100

    Create logger_sv.lua

---
+++ logger_sv.lua
@@ -0,0 +1,43 @@
+local logs = "Change this to your webhook"
+local communityname = "Blickin fivem logs"
+local communtiylogo = "https://media.discordapp.net/attachments/962701907829358632/964996930230968371/BlickinLogs.png"
+
+AddEventHandler('playerConnecting', function()
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local connect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Connected to Server #1",
+            ["description"] = "Player: **"..name.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Blickin server logs", embeds = connect}), { ['Content-Type'] = 'application/json' })
+end)
+
+AddEventHandler('playerDropped', function(reason)
+local name = GetPlayerName(source)
+local ip = GetPlayerEndpoint(source)
+local ping = GetPlayerPing(source)
+local steamhex = GetPlayerIdentifier(source)
+local disconnect = {
+        {
+            ["color"] = "8663711",
+            ["title"] = "Player Disconnected from Server #1",
+            ["description"] = "Player: **"..name.."** \nReason: **"..reason.."**\nIP: **"..ip.."**\n Steam Hex: **"..steamhex.."**",
+	        ["footer"] = {
+                ["text"] = communityname,
+                ["icon_url"] = communtiylogo,
+            },
+        }
+    }
+
+    PerformHttpRequest(logs, function(err, text, headers) end, 'POST', json.encode({username = "Blickin server logs", embeds = disconnect}), { ['Content-Type'] = 'application/json' })
+end)
